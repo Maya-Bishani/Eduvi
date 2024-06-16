83cde94f7f12d318f2d6d65ddaa3beeac42fd907
Naghamesmael <nagham.esmael70@gmail.com>
2024-06-16 17:55:28 +0300

    add hero and section3

---
+++ src/Pages/HomePage/Section3/Section3.jsx
@@ -0,0 +1,19 @@
+import './Section3.css'
+export default function Homesection3() {
+    return (
+        <section className='ne-section3'>
+
+            <div className='ne-container-left'>
+                <div className='ne-coleglevel'><p>College Level</p></div>
+                <p className='ne-covid'>Dont waste time in COVID-19 pandemic. Develop your skills.</p>
+                <p className='ne-section3p'>High-difination vidio is vidio ofhigher resolution and quality than stndarad-definintion. While there standerdized meaning for high-difination , generaly any vidio.</p>
+                <button className='ne-section3-but'>Registation Now</button>
+
+
+            </div>
+            <div className='ne-section3-right'>
+            <img src='./images/Homepage/Hero/lap.png'></img>
+            </div>
+        </section>
+    )
+}
