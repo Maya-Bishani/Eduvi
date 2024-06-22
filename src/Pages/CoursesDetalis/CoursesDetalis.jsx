51c6ec754a48a31306c2c6e0a99836301a36b0ee
Maya_Bishani <mayabishani6@gmail.com>
2024-06-22 12:36:34 +0300

    add by maya

--- src/Pages/CoursesDetalis/CoursesDetalis.jsx
+++ src/Pages/CoursesDetalis/CoursesDetalis.jsx
@@ -1,7 +1,9 @@
 import DetalisSection2 from './DetalisSection2/DetalisSection2'
+import Section1 from './Section1/Section1'
 export default function DetalisSecton2(){
     return(
         <>
+        <Section1/>
         <DetalisSection2/>
         </>
     )
