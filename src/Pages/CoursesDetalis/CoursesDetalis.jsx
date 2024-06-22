51076a78caca4d648117b2c23c5776ab2da7cb50
Maya_Bishani <mayabishani6@gmail.com>
2024-06-22 21:23:23 +0300

    add files by nagham maya syjal

--- src/Pages/CoursesDetalis/CoursesDetalis.jsx
+++ src/Pages/CoursesDetalis/CoursesDetalis.jsx
@@ -1,18 +1,15 @@
 
 import { CourseList } from "./CourseList/CourseList";
+import DetalisSection2 from './DetalisSection2/DetalisSection2'
+import Section1 from './Section1/Section1'
 
 export default function CoursesDetalis(){
     return(
         <>
         <CourseList/>
-
-import DetalisSection2 from './DetalisSection2/DetalisSection2'
-import Section1 from './Section1/Section1'
-export default function DetalisSecton2(){
-    return(
-        <>
         <Section1/>
         <DetalisSection2/>
+
 
         </>
     )
