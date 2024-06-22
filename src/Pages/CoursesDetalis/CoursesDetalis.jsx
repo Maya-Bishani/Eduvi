27dc6066406094746329fcdeda6fb3b95e8e5372
Syj152al2023 <syjalldyieab@gmail.com>
2024-06-22 23:41:34 +0300

    Hero of Courses Details/ resposive BreakPoints

--- src/Pages/CoursesDetalis/CoursesDetalis.jsx
+++ src/Pages/CoursesDetalis/CoursesDetalis.jsx
@@ -1,19 +1,13 @@
-
+import DetalisSection2 from './DetalisSection2/DetalisSection2'
+import Section1 from './Section1/Section1'
 import { CourseList } from "./CourseList/CourseList";
 
 export default function CoursesDetalis(){
     return(
         <>
         <CourseList/>
-
-import DetalisSection2 from './DetalisSection2/DetalisSection2'
-import Section1 from './Section1/Section1'
-export default function DetalisSecton2(){
-    return(
-        <>
-        <Section1/>
+<Section1/>
         <DetalisSection2/>
-
-        </>
+</>
     )
 }
