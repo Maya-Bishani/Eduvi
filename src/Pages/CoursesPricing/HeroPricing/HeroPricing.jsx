bafd9aac6c01437362655435a3046206307db6e7
Naghamesmael <nagham.esmael70@gmail.com>
2024-07-09 22:24:44 +0300

    qwertyuiop

--- src/Pages/CoursesPricing/HeroPricing/HeroPricing.jsx
+++ src/Pages/CoursesPricing/HeroPricing/HeroPricing.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
 import './HeroPricing.css'
 export default function HeroPricing(){
     return(       <>
        
        <div className='ne-hero-cours'>
             <div className='ne-home-cours'>
-                <div className='ne-herodiv1'>Hero</div>
-                <div className='ne-herodiv2' >pricing</div>
+                <div className='ne-herodiv1'>
+                <Link to ='/'> Hero</Link>
+                    </div>
+                <div className='ne-herodiv2' >
+                <Link to ='/CoursesPricing'>  pricing </Link>  
+                    </div>
             </div>
             <div className='ne-parent-img-p'>
                 <p className='ne-eduvi-courses'>Our Pre-ready Pricing Packages</p>
